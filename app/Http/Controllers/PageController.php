2bd1008ae4e52cb922058cd27b699ca42f94501b
Unknown <thangytp@gmail.com>
2018-11-08 01:30:38 +0700

    update route and controller for home

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function index()
+    {
+        return view('pages.home');
+    }
+}
