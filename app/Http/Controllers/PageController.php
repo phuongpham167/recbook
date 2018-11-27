0ee813003fa63d363d8acd489b29e00fd4f46116
Unknown <thangytp@gmail.com>
2018-11-28 01:25:36 +0700

    route and function for route danh-muc

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\MappingMenuFE;
 use App\Menu;
+use App\RealEstate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PageController extends Controller
 {
@@ -20,5 +23,30 @@ class PageController extends Controller
     {
         $menuData = $this->menuFE;
         return v('pages.home', compact('menuData'));
+    }
+
+    public function getDanhmuc($tag)
+    {
+        try {
+            $web_id = get_web_id();
+            $mappingMenuFEByTag = MappingMenuFE::where('path', $tag)->first();
+//            dd($mappingMenuFEByTag);
+            if ($mappingMenuFEByTag) {
+                $query = DB::table('real_estates')->whereNull('deleted_at')->where('web_id', $web_id);
+                if ($mappingMenuFEByTag->re_category_id) {
+                    $query->where('re_category_id', $mappingMenuFEByTag->re_category_id);
+                }
+                if ($mappingMenuFEByTag->re_category_id && $mappingMenuFEByTag->re_type_id) {
+                    $query->where('re_type_id', $mappingMenuFEByTag->re_type_id);
+                }
+//                if ($mappingMenuFEByTag->suggest) {
+//                    $query->where('');
+//                }
+                $results = $query->get();
+                dd($results);
+            }
+        } catch (\Exception $exception) {
+
+        }
     }
 }
