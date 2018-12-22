77340511e6108647d7840de586c4c0e56a7fc0e6
Unknown <thangytp@gmail.com>
2018-12-22 16:29:43 +0700

    update conversation controller

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -73,15 +73,25 @@ class ConversationController extends Controller
     public function store(Request $request){
         $c1 = Conversation::where('user1',Auth::user()->id)->where('user2',$request->user_id)->count();
         $c2 = Conversation::where('user2',Auth::user()->id)->where('user1',$request->user_id)->count();
-        if($c1 != 0 || $c2 != 0)
+        if($c1 != 0 || $c2 != 0) {
+            if ($c1) {
+                $conv = Conversation::where('user1',Auth::user()->id)->where('user2',$request->user_id)->first();
+                return redirect()->route('conversation.show', [$conv->id]);
+            }
+            if ($c2) {
+                $conv = Conversation::where('user2',Auth::user()->id)->where('user1',$request->user_id)->first();
+                return redirect()->route('conversation.show', [$conv->id]);
+            }
             return redirect()->back();
+        }
 
         $c = new Conversation();
         $c->user1 = Auth::user()->id;
         $c->user2 = $request->user_id;
         $c->save();
 
-        return redirect()->back();
+        return redirect()->route('conversation.show', [$c->id]);
+//        return redirect()->back();
     }
     public function show($id){
         $conversation = Conversation::findOrFail($id);
