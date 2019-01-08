21f98c19df14d7e069776757d94f9040d94be87c
Unknown <thangytp@gmail.com>
2019-01-09 02:04:18 +0700

    update user info

--- resources/views/themes/default/includes/user-info-header.blade.php
+++ resources/views/themes/default/includes/user-info-header.blade.php
@@ -1,48 +1,3 @@
-{{-- top page --}}
-<div class="top_page">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-8 welcome-text hidden-xs">
-                <p>chào mừng quý khách đến với đô thị group - hotline: <span>0989.186.179</span></p>
-            </div>
-
-            <div class="col-xs-12 col-sm-4 user-action">
-                @if (!auth()->check())
-                    <p class="pull-right">
-                        <a href="{{ route('login') }}"><i class="fa fa-sign-in"></i> ĐĂNG NHẬP</a>
-                        <a href="{{ route('register') }}"><i class="fa fa-user-plus"></i> ĐĂNG KÝ</a>
-                    </p>
-                @else
-                    <p class="pull-right">
-                        <a href="{{ route('recharge') }}"><i class="fa fa-credit-card"></i> <strong>Số dư: {{auth()->user()->credits}}</strong></a>
-                        <a href="{{ route('info') }}"><i class="fa fa-user"></i> <strong>{{auth()->user()->name}}</strong></a>
-                        <a href="{{ route('logout') }}"><i class="fa fa-sign-out"></i> ĐĂNG XUẤT</a>
-                    </p>
-                @endif
-            </div>
-        </div>
-    </div>
-</div>
-{{-- end to page --}}
-{{-- banner header --}}
-<div class="banner-header">
-    <div class="container">
-        <div class="row">
-            <div class="left col-xs-12 col-sm-4">
-                <a href="{{ route('home') }}">
-                    <img src="{{ asset('images/logo.png') }}" class="img-responsive" alt=""/>
-                </a>
-            </div>
-            <div class="right col-sm-8 hidden-xs">
-                <a href="">
-                    <img src="{{ asset('images/banner/banner-header.gif') }}" class="img-responsive" alt="" />
-                </a>
-            </div>
-            {{--<div class="clearfix"></div>--}}
-        </div>
-    </div>
-</div>
-{{-- end banner header --}}
 <nav class="navbar navbar-inverse main-menu main-menu-user-info">
     <div class="container">
         <div class="navbar-header">
@@ -55,7 +10,7 @@
         </div>
         <div class="collapse navbar-collapse main-menu-list" id="myNavbar">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="{{ route('home') }}"><i class="fa fa-home fa-lg fa-fw"></i> {{ trans('header.navbar-item.home') }}</a></li>
+                <li class="active"><a href="{{ route('home') }}"><i class="fa fa-home fa-lg fa-fw"></i></a></li>
                 <li>
                     <form class="menu-search" action="{{route('search')}}" method="get">
                         <div class="pull-right wrap">
@@ -75,6 +30,7 @@
                             <img class="img-responsive header-avatar" src="{{$avatar}}" > <span class="header-name">{{auth()->user()->userinfo->full_name}}</span>
                         </a>
                     </li>
+                    <li class="home-link"><a href="{{ route('home') }}">{{ trans('header.navbar-item.home') }}</a></li>
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="#" title="Lời mời kết bạn"><i class="fa fa-users" aria-hidden="true"></i></a>
                         <ul class="dropdown-menu friend-request-list">
--- resources/views/themes/default/users/user-info.blade.php
+++ resources/views/themes/default/users/user-info.blade.php
@@ -22,7 +22,7 @@
     {{-- Include Header --}}
     @include(theme(TRUE).'.includes.user-info-header')
     <div class="content-body">
-        <div class="container padding-top-30 padding-bottom-30">
+        <div class="container padding-bottom-30">
             <div class="row">
                 <div class="col-xs-12 col-md-12 detail-content-wrap">
                     {{--<p class="title_box"><strong>{{ $data->userinfo->full_name }}</strong>--}}
@@ -30,8 +30,14 @@
                     <div class="row">
                         <div class="col-xs-12 col-md-9">
                             <div class="detail-top">
-                                <div class="row">
-                                    <div class="col-xs-12">
+                                <div class="row row-wrap" >
+                                    <div class="col-xs-12 cover-wrap">
+                                        <div class="cover-content-wrap">
+                                            {{--<img src="http://thuthuat123.com/uploads/2018/01/27/tong-hop-anh-bia-facebook-dep-50_101149.jpg" class="img-responsive" />--}}
+                                        </div>
+                                        <button class="btn btn-default btn-change-cover"><i class="fa fa-camera"></i> Thêm banner</button>
+                                    </div>
+                                    <div class="col-xs-12 av-and-name-wrap">
                                         @php
                                             $avatar = $data->userinfo->avatar ? $data->userinfo->avatar : '/images/default-avatar.png';
                                         @endphp
