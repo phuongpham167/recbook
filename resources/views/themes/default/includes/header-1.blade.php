da8461ecb621039cc05123405ab1563d54175fe2
DESKTOP-TJB52LH\tuan3 <tuan3162@gmail.com>
2019-01-17 20:50:51 +0700

    rollback trang home

---
+++ resources/views/themes/default/includes/header-1.blade.php
@@ -0,0 +1,89 @@
+{{-- top page --}}
+<div class="top_page">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 welcome-text hidden-xs">
+                <p>chào mừng quý khách đến với đô thị group - hotline: <span>0989.186.179</span></p>
+            </div>
+
+            <div class="col-xs-12 col-sm-4 user-action">
+                @if (!auth()->check())
+                    <p class="pull-right">
+                        <a href="{{ route('login') }}"><i class="fa fa-sign-in"></i> ĐĂNG NHẬP</a>
+                        <a href="{{ route('register') }}"><i class="fa fa-user-plus"></i> ĐĂNG KÝ</a>
+                    </p>
+                @else
+                    <p class="pull-right">
+                        <a href="{{ route('recharge') }}"><i class="fa fa-credit-card"></i> <strong>Số dư: {{auth()->user()->credits}}</strong></a>
+                        <a href="{{ route('info') }}"><i class="fa fa-user"></i> <strong>{{auth()->user()->name}}</strong></a>
+                        <a href="{{ route('logout') }}"><i class="fa fa-sign-out"></i> ĐĂNG XUẤT</a>
+                    </p>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
+{{-- end to page --}}
+{{-- banner header --}}
+<div class="banner-header">
+    <div class="container">
+        <div class="row">
+            <div class="left col-xs-12 col-sm-4">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('images/logo.png') }}" class="img-responsive" alt=""/>
+                </a>
+            </div>
+            <div class="right col-sm-8 hidden-xs">
+                <a href="">
+                    <img src="{{ asset('images/banner/banner-header.gif') }}" class="img-responsive" alt="" />
+                </a>
+            </div>
+            {{--<div class="clearfix"></div>--}}
+        </div>
+    </div>
+</div>
+{{-- end banner header --}}
+<nav class="navbar navbar-inverse main-menu">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand visible-xs" href="{{ route('home') }}">DoThiGroup</a>
+        </div>
+        <div class="collapse navbar-collapse main-menu-list" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="{{ route('home') }}"><i class="fa fa-home fa-lg fa-fw"></i> {{ trans('header.navbar-item.home') }}</a></li>
+                @php
+                    $menuData = json_decode($menuData->data);
+                @endphp
+                @foreach($menuData as $md)
+                    @if (isset($md->children) && $children = $md->children)
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{ $md->name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                @foreach($children as $child)
+                                    <li><a href="{{ route('danh-muc', ['tag' => $child->path]) }}">{{ $child->name }}</a></li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @else
+                        <li class=""><a href="{{ route('danh-muc', ['tag' => $md->path]) }}"> {{ $md->name }}</a></li>
+                    @endif
+                @endforeach
+                <li class=""><a href="{{ route('contact') }}"> {{ trans('menu.contact') }}</a></li>
+                <li class=""><a href="{{ route('get.create-real-estate') }}">{{ trans('menu.create_real_estate') }}</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <form class="menu-search" action="{{route('search')}}" method="get">
+                    <div class="pull-right wrap">
+                        <input id="ip-kw" name="txtkeyword" class="form-control pull-left" type="text" placeholder="{{trans('system.searchPlaceholder')}}">
+                        <button type="submit" class="pull-left"><i class="fa fa-search"></i></button>
+                    </div>
+                </form>
+            </ul>
+        </div>
+    </div>
+</nav>
