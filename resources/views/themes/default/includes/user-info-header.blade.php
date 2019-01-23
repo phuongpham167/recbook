a0c82d6221b2cdce70c57c49276b4d480f9284f9
DESKTOP-GM90TEF\admin <beehomiez@gmail.com>
2019-01-23 18:09:53 +0700

    update notice

--- resources/views/themes/default/includes/user-info-header.blade.php
+++ resources/views/themes/default/includes/user-info-header.blade.php
@@ -43,7 +43,7 @@
                                 <li><a>{{$authFriendRequest->fuser1->userinfo->full_name}}</a> <a href="{{route('friend.confirm.request', [$authFriendRequest->fuser1->id])}}" class="btn btn-primary pull-right btn-accept-rq"><i class="fa fa-plus"></i> Chấp nhận</a></li>
                                 @endforeach
                             @else
-                                <li><a class="dropdown1">Không có lời mời kết bạn nào</a></li>
+                                <li><a class="notice_dropdown">Không có lời mời kết bạn nào</a></li>
                             @endif
                         </ul>
                     </li>
@@ -51,72 +51,28 @@
                         {{--<a href="{{ route('chat') }}" title="Tin nhắn"><i class="fa fa-comment" aria-hidden="true"></i></a>--}}
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true" title="Tin nhắn"><i class="fa fa-comment" aria-hidden="true"></i>
                         </a>
-                        <ul class="dropdown-menu">
+                        <ul class="dropdown-menu message_dropdown">
                             <li class="header">You have 4 messages</li>
                             <li>
                                 <!-- inner menu: contains the actual data -->
                                 <ul>
-                                    <li><!-- start message -->
-                                        <a class="dropdown1" href="#">
-                                            <div class="pull-left">
-                                                <img width="50px" height="50px" src="/images/default-avatar.png" class="img-circle" alt="User Image">
+                                    @foreach(\App\Conversation::orderBy('created_at', 'desc')->where(function ($q) {
+                                        $q->where('user1',auth()->user()->id)->orWhere('user2',auth()->user()->id);
+                                    })->take(5)->get() as $item)
+                                        <li>
+                                            <div class="row">
+                                                <div class="col-md-3">
+                                                    <a href="#a"><img width="50px" height="50px" src="/images/default-avatar.png" class="img-circle" alt="User Image"></a>
+                                                </div>
+                                                <div class="col-md-9">
+                                                    <p><a class="notice_dropdown" href="#a">
+                                                            <?php $message =  \App\Message::orderBy('created_at', 'desc')->take(1)->first()?>
+                                                            {{\App\User::find($message->user_id)->name}}<small style=" color: #cacaca"> <i class="fa fa-clock-o"></i> 5 mins</small><p>{{$message->text}}</p>
+                                                    </a></p>
+                                                </div>
                                             </div>
-                                            <h4>
-                                                Support Team
-                                                <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                                            </h4>
-                                            <p>Why not buy a new awesome theme?</p>
-                                        </a>
-                                    </li>
-                                    <!-- end message -->
-                                    <li>
-                                        <a class="dropdown1" href="#">
-                                            <div class="pull-left">
-                                                <img width="50px" height="50px" src="/images/default-avatar.png" class="img-circle" alt="User Image">
-                                            </div>
-                                            <h4>
-                                                AdminLTE Design Team
-                                                <small><i class="fa fa-clock-o"></i> 2 hours</small>
-                                            </h4>
-                                            <p>Why not buy a new awesome theme?</p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown1" href="#">
-                                            <div class="pull-left">
-                                                <img width="50px" height="50px" src="/images/default-avatar.png" class="img-circle" alt="User Image">
-                                            </div>
-                                            <h4>
-                                                Developers
-                                                <small><i class="fa fa-clock-o"></i> Today</small>
-                                            </h4>
-                                            <p>Why not buy a new awesome theme?</p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown1" href="#">
-                                            <div class="pull-left">
-                                                <img width="50px" height="50px" src="/images/default-avatar.png" class="img-circle" alt="User Image">
-                                            </div>
-                                            <h4>
-                                                Sales Department
-                                                <small><i class="fa fa-clock-o"></i> Yesterday</small>
-                                            </h4>
-                                            <p>Why not buy a new awesome theme?</p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown1" href="#">
-                                            <div class="pull-left">
-                                                <img width="50px" height="50px" src="/images/default-avatar.png" class="img-circle" alt="User Image">
-                                            </div>
-                                            <h4>
-                                                Reviewers
-                                                <small><i class="fa fa-clock-o"></i> 2 days</small>
-                                            </h4>
-                                            <p>Why not buy a new awesome theme?</p>
-                                        </a>
-                                    </li>
+                                        </li>
+                                    @endforeach
                                 </ul>
                             </li>
                             <li class="footer"><a href="#">See All Messages</a></li>
@@ -140,20 +96,13 @@
     </div>
 </nav>
 <style>
-   .dropdown-menu>li .menu>li>a {
+   .dropdown-menu>li{
         margin: 0;
         padding: 10px 10px;
-    }
-    .dropdown-menu>li .menu>li>a, .dropdown-menu>li .menu>li>a, .dropdown-menu>li .menu>li>a {
+   }
+   .dropdown-menu>li .menu>li>a, .dropdown-menu>li .menu>li>a, .dropdown-menu>li .menu>li>a {
         display: block;
         white-space: nowrap;
         border-bottom: 1px solid #f4f4f4;
-    }
-    .dropdown-menu>li h4 p{
-        padding: 0;
-        margin: -35px 0 0 60px;
-        color: #444444;
-        font-size: 15px;
-        position: relative;
-    }
+   }
 </style>
