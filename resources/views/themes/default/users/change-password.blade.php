f41ba9e65d7db5376e2e75354709e086eafac6b9
Unknown <thangytp@gmail.com>
2019-03-03 04:25:48 +0700

    change header

--- resources/views/themes/default/users/change-password.blade.php
+++ resources/views/themes/default/users/change-password.blade.php
@@ -13,7 +13,7 @@
 @endpush
 
 @section('content')
-    @include(theme(TRUE).'.includes.user-info-header')
+    @include(theme(TRUE).'.includes.user-info-header-1')
     <div class="container-vina">
         <div class="row subpage">
 
