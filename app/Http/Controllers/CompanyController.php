abf16e631b42dba869b25f1f3ce0b3e411219a32
Tuấn Đinh <tuan3162@gmail.com>
2019-03-19 16:03:42 +0700

    hàm data

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -66,4 +66,20 @@ class CompanyController extends Controller
         }else
             return v('company.confirm', compact('data', 'confirmed'));
     }
+
+    public function realEstateData()
+    {
+
+        $customer = Customer::where('user_id', auth()->user()->id)->pluck('id')->toArray();
+        $data   =   RealEstate::whereIn('customer_id', $customer)->withoutGlobalScope(PrivateScope::class);
+        $result = Datatables::of($data)
+            ->addColumn('title', function(RealEstate $item){
+                return "<a href='".route('customerCare', ['id'=>$item->customer_id,'re_id' => $item->id])."'>".$item->title."</a>";
+            })
+            ->addColumn('type', function(RealEstate $item){
+                return $item->reType?$item->reType->name:'';
+            })->rawColumns(['title']);
+
+        return $result->make(true);
+    }
 }
