227df448bf0cbe0333a15262416fea5d2f1eed03
Tuấn Đinh <tuan3162@gmail.com>
2019-03-20 22:29:58 +0700

    fix tab

--- resources/views/themes/default/includes/company_customer_manager_tabs.blade.php
+++ resources/views/themes/default/includes/company_customer_manager_tabs.blade.php
@@ -4,6 +4,9 @@
         <li role="presentation" @if(request()->route()->getName() == 'companyDetail') class="active" @endif><a class="freelancer_tab" href="{{route('companyDetail',['id'=>$company_id])}}">Quản lý thành viên</a></li>
         <li role="presentation" @if(request()->route()->getName() == 'companyGroupList') class="active" @endif><a class="freelancer_tab" href="{{route('companyGroupList', ['id'=>$company_id])}}">Quản lý nhóm</a></li>
     @endif
+    @if(get_role($company_id) == 'manager')
+        <li role="presentation" @if(request()->route()->getName() == 'companyGroupDetail') class="active" @endif><a class="freelancer_tab" href="{{route('companyGroupDetail', ['id'=>find_group($company_id)->id])}}">Quản lý nhóm</a></li>
+    @endif
     <li role="presentation" @if(url()->current() == asset('doanh-nghiep/khach-hang')) class="active" @endif><a class="freelancer_tab" href="{{asset('doanh-nghiep/khach-hang?id='.request('id'))}}">Danh sách khách hàng</a></li>
 
     <li role="presentation" @if(url()->current() == asset('doanh-nghiep/yeu-cau')) class="active" @endif><a class="freelancer_tab" href="{{asset('doanh-nghiep/yeu-cau')}}">Quản lý yêu cầu</a></li>
